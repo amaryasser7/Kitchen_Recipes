b6ce777eab02d34e728c1cbb5c936b930739975b
amaryasser7 <yamar8861@gmail.com>
2025-11-01 22:22:59 +0200

    player animation

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class player_script : MonoBehaviour
 {
+    private bool isWalking;
     [SerializeField] private float speed = 7f;
+    [SerializeField] private Animator animator;
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -36,15 +38,24 @@ public class player_script : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * speed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+        if (isWalking)
+        { animator.SetBool("IsWalking", true); }
+        else
+        { animator.SetBool("IsWalking", false); }
+
         float RotationSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, RotationSpeed * Time.deltaTime);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, RotationSpeed * Time.deltaTime);   
         }
 
 
 
 
 
-
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
 
 
 
