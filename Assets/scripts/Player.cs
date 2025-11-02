24c5f556a5d027519cee8b4546b6908dfdc726df
amaryasser7 <yamar8861@gmail.com>
2025-11-03 01:19:46 +0200

    Input system refactor

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,61 +5,32 @@ using UnityEngine;
 public class player_script : MonoBehaviour
 {
     private bool isWalking;
-    [SerializeField] private float speed = 7f;
+    [SerializeField] private float move_speed = 7f;
     [SerializeField] private Animator animator;
+    [SerializeField ] private GameInput gameInput;
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();  
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * speed * Time.deltaTime;
+ 
+        transform.position += moveDir * move_speed * Time.deltaTime;
+
 
         isWalking = moveDir != Vector3.zero;
+        
         if (isWalking)
         { animator.SetBool("IsWalking", true); }
         else
         { animator.SetBool("IsWalking", false); }
 
         float RotationSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, RotationSpeed * Time.deltaTime);   
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, RotationSpeed * Time.deltaTime);
+
         }
-
-
-
-
 
     public bool IsWalking()
     {
         return isWalking;
     }
-
-
-
-
 
 
 }
