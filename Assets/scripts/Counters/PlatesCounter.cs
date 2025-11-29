9926a90074ce9449a9f03ea0bf9a851467201ed9
amaryasser7 <yamar8861@gmail.com>
2025-11-29 11:39:04 +0200

    Add plate prefab and visual logic to PlatesCounter
    Introduced a Plate prefab and its ScriptableObject, updated PlatesCounter to inherit from BaseCounter, and added event-driven plate visual spawning via the new PlateCouunterVisual script. Updated GameScene and PlatesCounter prefab to integrate the new plate system and visuals.

--- Assets/scripts/Counters/PlatesCounter.cs
+++ Assets/scripts/Counters/PlatesCounter.cs
@@ -1,17 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatesCounter : MonoBehaviour
+public class PlatesCounter : BaseCounter
 {
+
+    public event EventHandler OnPlateSpawned;
+
+
     [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
 
     private float spwanPlateTimer;
     private float spwanPlateTimerMax = 4f;
+    private int plateSpawnedAmount;
+    private int plateSpwanAmountMax = 4;
 
     private void Update()
     {
         spwanPlateTimer +=Time.deltaTime;
-        if  
+        if  (spwanPlateTimer > spwanPlateTimerMax)
+        {
+            spwanPlateTimer = 0f;
+
+            if(plateSpawnedAmount < plateSpwanAmountMax)
+            {
+                plateSpawnedAmount ++ ;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        
     }
 }
